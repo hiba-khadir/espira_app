bb57dfa5be90d2bfcd5a94874f16954154dea6a6
hiba_khadir <khadirhiba0745@gmail.com>
2026-04-11 00:04:04 +0100

    basic server setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,28 +5,28 @@ const path = require("path");
 
 //user authentification router
 const authRoute = require("./routes/auth.router.js");
-const authenticateToken = require("./middleware/auth.middleware.js");
+const authenticateToken = require("./middleware/auth.middelware.js");
 
 
-//products router
-const productRoutes = require("./routes/product.router.js");
-const productAdminRoutes = require('./routes/admin/product.admin.router.js');
+//devices router
+const deviceRoutes = require("./routes/device.router.js");
 const app = express();
 
 //stats router
-const adminStatsRoute = require("./routes/admin/stats.admin.router.js");
+
 
 //frontend connection
+/*
 app.use(cors({
     origin: 'http://localhost:5173'  //accept requests from the app's frontend only
-}));
+}));*/
 
-//extend files limit 
+//extend files limit and use expess.json 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-
-app.use(express.json());
+//defining api endpoints 
+app.use("/api/devices",authenticateToken,deviceRoutes);
 
 
 app.listen(3000, () => console.log("Server running on 3000"));
