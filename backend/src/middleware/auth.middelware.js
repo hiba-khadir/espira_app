3411ea405f6b1a3c3fd12ffd47ea5bb4330aef9e
hiba_khadir <khadirhiba0745@gmail.com>
2026-04-11 00:02:52 +0100

    basic jwt middleware for token verification

--- backend/src/middleware/auth.middelware.js
+++ backend/src/middleware/auth.middelware.js
@@ -1,23 +1,21 @@
-import jwt from "jsonwebtoken";
-
 const jwt = require('jsonwebtoken');
 require('dotenv').config({override:true});
 
-//generate 
+//authenticate token : verifies if access token send with request is valid
 function authenticateToken (req ,res ,next){
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];  //token field
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) return res.status(401);
+    if (!token) return res.status(401).send('No token was sent');
 
     //else verify token 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET ,
-        (err, pl)=>{
-            if (err) 
-                return res.status(403).send('invalid token');
-            req.pl = pl;
-            next();
-        }
-    )
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)  // verifies signature + expiry
+        req.user = { id: payload.sub }  //get id from verified token & attach to req
+        next()
+    }catch {
+        res.status(401).json({ error: 'Invalid token' })
+    }
 }
+
 module.exports = authenticateToken;
