48d11d663eb94feb8b24fcca635303a5018ccf1b
hiba_khadir <khadirhiba0745@gmail.com>
2026-04-20 18:07:14 +0100

    stats routes tested and working

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-
-
+import { runHistoryCleanup } from './services/historyCleanup';
 /*routes and auth */
 import authenticateToken from './middleware/auth.middleware.js';
 import deviceRoutes from './routes/device.router.js';
 import authRoute from "./routes/auth.router.js";
+import statsRoutes from './routes/stats.router';
 
 const app = express();
-
-//stats router
-
 
 //frontend connection
 app.use(cors({
@@ -24,6 +21,11 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //defining api endpoints 
 app.use("/api/devices",authenticateToken,deviceRoutes);
+app.use("/api/stats",authenticateToken,statsRoutes);
 
 
+
+// run cleaning old history once on startup then every 24 hours
+runHistoryCleanup();
+setInterval(runHistoryCleanup, 24 * 60 * 60 * 1000);
 app.listen(3000, () => console.log("Server running on 3000"));
